5bf74c35d48778a811b7dceb0d0cab3d8a37ad99
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2023-09-07 05:24:28 +0000

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className=' bg-primary py-12 text-white'>
       <div className=' container mx-auto'>
         <p className='text-white text-center'>
-          Copyright &copy; Ecommerce shop. All rights reserved.
+          Copyright &copy; AI Powered Shop. All rights reserved.
         </p>
       </div>
     </footer>
