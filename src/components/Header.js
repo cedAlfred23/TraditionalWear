bc80dd341dc955e60f093938e99a5c191d36462d
cedAlfred23 <al.cedricgbaguidi@gmail.com>
2023-09-07 04:56:03 +0000

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
+import { CartContext } from '../contexts/CartContext';
+import {BsBag} from 'react-icons/bs';
+import {Link} from 'react-router-dom'
+import Logo from '../img/logo.svg'
 
 const Header = () => {
-  return <div>Header</div>;
+  const [isActive, setIsActive] = useState(false);
+  const{ isOpen, setIsOpen } = useContext(SidebarContext);
+  const{ itemAmount } = useContext(CartContext);
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=> {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    });
+  });
+  return (
+    <header className={`${isActive ? 'bg-white py-4 shadow-md' : 'bg-none py-6'} fixed w-full z-10 transition-all`}>
+      <div className='container flex flex-1 justify-between items-center h-full mx-auto py-[10px]'>
+    <Link to={'/'}>
+      <div className=''>
+        <img src={Logo} className=' w-[40px]'/>
+      </div>
+    </Link>
+    <div onClick={() => setIsOpen(!isOpen)} className=' cursor-pointer flex relative'>
+      <BsBag className='text-2xl' />
+      <div className=' absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[12px] text-white rounded-full flex justify-center items-center bg-red-500 p-2'>
+        {itemAmount}
+      </div>
+    </div>
+  </div>
+    </header>
+  )
 };
 
 export default Header;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import WomanImg from '../img/woman.png';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return <section className=' h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
+    <div className='container mx-auto flex justify-around h-full'>
+      <div className='flex flex-col p-14 justify-center m-3'>
+        <div className=' font-semibold flex items-center uppercase'>
+          <div className='w-10 h-[2px] bg-blue-500 mr-3 uppercase'></div>New Trend
+        </div>
+        <h1 className='text-[70px] leading-[1.1] font-light mb-4 uppercase'>
+          Your traditional Wear <br/>
+          <span className='font-semibold uppercase'> AI powered</span>
+        </h1>
+        <Link to={'/'} className='self-start uppercase font-semibold border-b-2 border-primary hover:border-b-0'>Discover more</Link>
+      </div>
+      <div>
+        <img src={WomanImg} className='hidden lg:block w-full h-[695px] items-end justify-end md:h-[500]'/>
+      </div>
+    </div>
+  </section>
 };
 
 export default Hero;
+ 
