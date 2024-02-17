8ad4269515b69d1f6da21161139e1a0f5bb82ffc
Raymond-ap <apunguray@gmail.com>
2024-02-17 22:39:45 +0000

    code refactoring

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,46 +1,75 @@
-import React, {useContext} from 'react';
-import { useParams } from 'react-router-dom';
-import { CartContext } from '../contexts/CartContext';
-import { ProductContext } from '../contexts/ProductContext';
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { CartContext } from "../contexts/CartContext";
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const { products } = useContext(ProductContext);
+
+  const { error, loading, products } = useFetch();
   const { addToCart } = useContext(CartContext);
 
-  const product = products.find((item) => {
-    return item.id === parseInt(id);
+  const product = products?.find((item) => {
+    return item?.id === parseInt(id);
   });
 
-  if(!product){
-    return <section className='h-screen flex justify-center items-center'>
-      Loading...
-    </section>
-  }
-
-  const {brand, price, shortDescription, img_path} = product;
-
-  return (
-    <section className=' pt-32 pb-12 lg:py-32 h-screen flex items-center'>
-      <div className='container mx-auto'>
-        <div className='flex flex-col lg:flex-row items-center'>
-          <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
-            <img src={img_path} className='max-w-[200px] lg:max-w-sm'/>
-          </div>
-          <div className='flex-1 text-center lg:text-left'>
-            <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>
-              {brand}
-            </h1>
-            <div className='text-xl text-red-500 font-medium mb-6'>
-            ¢ {price}
+  if (!product) {
+    return (
+      <section className=" pt-32 pb-12 lg:py-32 h-screen flex items-center">
+        <div className="container mx-auto">
+          <div role="status" class=" p-4  rounded animate-pulse md:p-6 ">
+            <div class="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+              <svg
+                class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 16 20"
+              >
+                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+              </svg>
             </div>
-            <p className='mb-8'>{shortDescription}</p>
-            <button onClick={()=> addToCart(product, product.id)} className=' text-yellow-500 bg-gray-700 py-4 px-8 mb-40 rounded'>Add to cart</button>
+            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+
+            <span class="sr-only">Loading...</span>
           </div>
         </div>
-      </div>    
+      </section>
+    );
+  }
+
+  const { category, price, description, image } = product;
+
+  return (
+    <section className=" pt-32 pb-12 lg:py-32 h-screen flex items-center">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row items-center">
+          <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
+            <img src={image} className="max-w-[200px] lg:max-w-sm" />
+          </div>
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0 capitalize">
+              {category}
+            </h1>
+            <div className="text-xl text-red-500 font-medium mb-6">
+              ¢ {price}
+            </div>
+            <p className="mb-8">{description}</p>
+            <button
+              onClick={() => addToCart(product, product.id)}
+              className=" text-yellow-500 bg-gray-700 py-4 px-8 mb-40 rounded"
+            >
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
-  )
+  );
 };
 
 export default ProductDetails;
