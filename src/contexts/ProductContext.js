9e99b831c2f6ebe99bb6638e993365886bd2e6c3
Raymond-ap <apunguray@gmail.com>
2024-02-17 22:39:39 +0000

    code refactoring

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -3,15 +3,18 @@ export const ProductContext = createContext();
 
 const ProductProvider = ({children}) => {
   const [products, setProducts] = useState([]);
+
+  const fetchProducts = async ()=> {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data =  await response.json();
+    console.log('Here we are ', data);
+    setProducts(data);
+  };
+
   useEffect(()=>{
-    const fetchProducts = async ()=> {
-      const response = await fetch('https://fakestoreapi.com/products');
-      const data =  await response.json();
-      console.log('Here we are ', data);
-      setProducts(data);
-    };
     fetchProducts();
   }, []);
+  
   return <ProductContext.Provider value={{ products }}>
     {children}
   </ProductContext.Provider>
