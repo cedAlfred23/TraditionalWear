0c292fe46f02da3f534dca089c3603edd84d8a84
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-02-19 15:54:49 +0000

    Update api url

--- src/util/api.js
+++ src/util/api.js
@@ -1,9 +1,9 @@
 /**
  * API TO FETCH ENDPOINT FOR FAKESTORE
  */
-export const PRODUCT_URL = "http://localhost:5000/api";
+export const PRODUCT_URL = "https://ff78-41-218-206-241.ngrok-free.app/api";
 
 /**
  * Detection API ENdpoint
  */
-export const DETECTION_URL = "http://localhost:5000/api/predict";
+export const DETECTION_URL = "https://ff78-41-218-206-241.ngrok-free.app/api/predict";
